3ce916b83e68b605f22180cb73cccc7525970fc2
RsanabriaZ <Owner@Rene.local>
2014-08-27 14:01:47 -0600

    Diseño email
    Mejorado el diseño del email.

--- app/views/emails/usuarioCreado.blade.php
+++ app/views/emails/usuarioCreado.blade.php
@@ -5,22 +5,45 @@
 	<style type="text/css">
 
 	body {
-		background-color: #ebeceb;
-		font-size: 1.1em;
 	}
 		.contenido{
-			background-color: #ffffff;
-			border-bottom-right-radius: 5px;
-			border-bottom-left-radius: 5px;
-			color: #454545;
-			border: 1px solid black;
-			border-top: 20px solid #000000;
-			line-height: 40px;
-			padding-left: 20px
+			
 
 		}
 
 		.iniciar{
+
+		}
+		.contenido .datos{
+			
+		}
+
+	</style>
+</head>
+<body style="
+		background-color: #ebeceb;
+		font-size: 1.1em;">
+
+<div class="contenido" style="background-color: #ffffff;
+			border-bottom-right-radius: 5px;
+			border-bottom-left-radius: 5px;
+			color: #454545;
+			border: 1px solid #454545;
+			border-top: 20px solid #000000;
+			line-height: 40px;
+			padding-left: 20px">
+
+<h2 class="usuario"> <strong>{{$usuario->nombre}}</strong> </h2>
+
+	
+	Estos son los datos de acceso para el sistema CDMYPE <br>
+	
+	<div class="datos" style="padding-left: 20px;">
+		Usuario: <strong> {{$usuario->email}} </strong><br>
+		Contraseña: <strong> {{$pass}} </strong> <br>
+	</div>
+
+	<a href="{{route('login')}}" class="iniciar" style="
 			padding-top: 10px;
 			padding-bottom: 10px;
 			padding-left: 30px;
@@ -30,29 +53,8 @@
 			color: #ffffff;
 			text-decoration: none;
 			width: 150px;
-
-		}
-		.contenido .datos{
-			padding-left: 20px;
-		}
-
-	</style>
-</head>
-<body>
-
-<div class="contenido">
-
-<h2 class="usuario"> <strong>{{$usuario->nombre}}</strong> </h2>
-
-	
-	Estos son los datos de acceso para el sistema CDMYPE <br><br>
-	
-	<div class="datos">
-		Usuario: <strong> {{$usuario->email}} </strong><br><br>
-		Contraseña: <strong> {{$pass}} </strong> <br><br>
-	</div>
-
-	<a href="{{route('login')}}" class="iniciar"> Entrar al Sistema</a> <br><br>
+			margin-bottom: 50px;
+			"> Entrar al Sistema</a> <br>
 
 </div>
 
